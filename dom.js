e6ffe728e54801e47fe5c0b9f3de4048849e269d
Alwaysram <Alwaysram@gmail.com>
2023-03-29 09:40:53 +0530

    adding sharpner file

---
+++ dom.js
@@ -0,0 +1,28 @@
+//Examine the document object
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+
+
+//getElementById
+//console.log(document.getElementById('header-title'));
+ var headerTitle=document.getElementById('header-title');
+ var header=document.getElementById('main-header');
+ var ul=document.getElementById('items');
+ var div=document.getElementById('main');
+
+// console.log(headerTitle);
+// headerTitle.textContent="head";
+// headerTitle.innerText="naresh";
+//headerTitle.innerHTML ='<h3>hello</h3>';
+header.style.border='solid 10px #000';
+ul.style.color='green';
+div.style.color='green';
+
+
+
+
