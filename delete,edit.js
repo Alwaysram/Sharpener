0d2bbd67bb2d3e0c2c4c82cc18a3277e19510ef9
Alwaysram <rc4663128@gmail.com>
2023-03-30 18:14:25 +0530

    filter

--- delete,edit.js
+++ delete,edit.js
@@ -63,6 +63,20 @@ function removeItem(e){
    }
 }
 ///fiter items
-function filterItems(){
-    //
+function filterItems(e){
+    //convert text to lowercase
+    var text=e.target.value.toLowerCase();
+    //get lis
+    var items = itemList.getElementsByTagName('li');
+    //convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!= -1){
+            item.style.display ='block';
+
+        }else{
+            item.style.display = 'none';
+        }
+    });
+
 }
